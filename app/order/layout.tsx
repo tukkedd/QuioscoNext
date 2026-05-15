2bf24bc31a14ed8747cd14b1d788d722cf1f6625
tukked <jeamki2501@gmail.com>
2026-05-15 17:36:14 -0400

    product cards, mapping and fixing styles

--- app/order/layout.tsx
+++ app/order/layout.tsx
@@ -7,7 +7,7 @@ export default function OrderLayout({ children}: Readonly<{children: React.React
             <div className="md:flex">
                 <OrderSidebar />
 
-                <main className="md:flex-1 md:h-screen md:overflow-y-scroll`">
+                <main className="md:flex-1 p-5 md:h-screen md:overflow-y-scroll">
                     {children}
                 </main>
 
